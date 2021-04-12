a958c38e260ea5418530aee9969c9bd3be0f3a8d
manderas20 <56639521+manderas20@users.noreply.github.com>
2021-04-12 18:40:09 +0100

    navbar
    navbar : routing  / icons

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import  Footer from './components/Footer';
-import Main from './components/Main';
+import React from 'react';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 
+import Footer from './components/Footer';
+import Main from './components/Main';
 
-export default class App extends Component {
-	render() {
-		return (
-			<div>
-				<Navbar/>
-				<Main/>
-				<Footer/>
-			</div>
-		)
-	}
+export default function App() {
+	return (
+		<div>
+			<Router>
+				<Navbar />
+				<Switch>
+					<Route path='/' exact component={Main} />
+				</Switch>
+				<Footer />
+			</Router>
+		</div>
+	);
 }
-
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,92 +1,198 @@
 import React, { Component } from 'react';
-
+import { IconContext } from 'react-icons';
+import { FaRegNewspaper } from 'react-icons/fa';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+	HiOutlineHome,
+	HiOutlineNewspaper,
+	HiOutlineSpeakerphone,
+	HiOutlinePhone,
+} from 'react-icons/hi';
+import { IoMegaphoneOutline } from 'react-icons/io5';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+	NavLink,
+} from 'react-router-dom';
+import { ImProfile } from 'react-icons/im';
+import { FaHome } from 'react-icons/fa';
+import { IoNewspaperSharp } from 'react-icons/io5';
 
-export default class navbar extends Component {
-    render() {
-        return (
-            <div>
-  <header className="main_menu home_menu">
-    <div className="container">
-      <div className="row align-items-center justify-content-center">
-        <div className="col-lg-12">
-          <nav className="navbar navbar-expand-lg navbar-light">
-            <a className="navbar-brand" href="index.html"> <img src="img/logo.png" alt="logo" /> </a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="menu_icon"><i className="fas fa-bars" /></span>
-            </button>
-            <div className="collapse navbar-collapse main-menu-item" id="navbarSupportedContent">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link" href="index.html">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="about.html">about</a>
-                </li>
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    product
-                  </a>
-                  <div className="dropdown-menu" aria-labelledby="navbarDropdown_1">
-                    <a className="dropdown-item" href="product_list.html"> product list</a>
-                    <a className="dropdown-item" href="single-product.html">product details</a>
-                  </div>
-                </li>
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_3" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    pages
-                  </a>
-                  <div className="dropdown-menu" aria-labelledby="navbarDropdown_2">
-                    <a className="dropdown-item" href="login.html"> 
-                      login
-                    </a>
-                    <a className="dropdown-item" href="checkout.html">product checkout</a>
-                    <a className="dropdown-item" href="cart.html">shopping cart</a>
-                    <a className="dropdown-item" href="confirmation.html">confirmation</a>
-                    <a className="dropdown-item" href="elements.html">elements</a>
-                  </div>
-                </li>
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="blog.html" id="navbarDropdown_2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    blog
-                  </a>
-                  <div className="dropdown-menu" aria-labelledby="navbarDropdown_2">
-                    <a className="dropdown-item" href="blog.html"> blog</a>
-                    <a className="dropdown-item" href="single-blog.html">Single blog</a>
-                  </div>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="contact.html">Contact</a>
-                </li>
-              </ul>
-            </div>
-            <div className="hearer_icon d-flex align-items-center">
-              <a id="search_1" href="javascript:void(0)"><i className="ti-search" /></a>
-              <a href="cart.html">
-                <i className="flaticon-shopping-cart-black-shape" />
-              </a>
-            </div>
-          </nav>
-        </div>
-      </div>
-    </div>
-    <div className="search_input" id="search_input_box">
-      <div className="container ">
-        <form className="d-flex justify-content-between search-inner">
-          <input type="text" className="form-control" id="search_input" placeholder="Search Here" />
-          <button type="submit" className="btn" />
-          <span className="ti-close" id="close_search" title="Close Search" />
-        </form>
-      </div>
-    </div>
-  </header>
-</div>
+export default function Navbar() {
+	return (
+		<div>
+			<header className='main_menu home_menu'>
+				<div className='container'>
+					<div className='row align-items-center justify-content-center'>
+						<IconContext.Provider
+							value={{
+								size: '22px',
+								style: { padding: '0px 4px 4px 0 ' },
+							}}
+						>
+							<div className='col-lg-12'>
+								<nav className='navbar navbar-expand-lg navbar-light'>
+									<NavLink className='navbar-brand' to='/' exact>
+										{' '}
+										<img src='img/logo.png' alt='logo' />{' '}
+									</NavLink>
+									<button
+										className='navbar-toggler'
+										type='button'
+										data-toggle='collapse'
+										data-target='#navbarSupportedContent'
+										aria-controls='navbarSupportedContent'
+										aria-expanded='false'
+										aria-label='Toggle navigation'
+									>
+										<span className='menu_icon'>
+											<i className='fas fa-bars' />
+										</span>
+									</button>
+									<div
+										className='collapse navbar-collapse main-menu-item'
+										id='navbarSupportedContent'
+									>
+										<ul className='navbar-nav'>
+											<li className='nav-item'>
+												<NavLink
+													className='nav-link'
+													to='/'
+													exact
+													activeStyle={{
+														color: '#795376 ',
+														borderBottom: '3px solid #795376',
+													}}
+												>
+													<HiOutlineHome /> Home
+												</NavLink>
+											</li>
+											<li className='nav-item'>
+												<NavLink
+													className='nav-link'
+													to='/about'
+													exact
+													activeStyle={{
+														color: '#795376 ',
+														borderBottom: '3px solid #795376',
+													}}
+												>
+													<FaRegNewspaper />
+													about
+												</NavLink>
+											</li>
+											<li className='nav-item dropdown'>
+												<NavLink
+													className='nav-link dropdown-toggle'
+													to='/Services'
+													id='navbarDropdown_1'
+													role='button'
+													data-toggle='dropdown'
+													aria-haspopup='true'
+													aria-expanded='false'
+													activeStyle={{
+														color: '#795376 ',
+														borderBottom: '3px solid #795376',
+													}}
+												>
+													<HiOutlinePhone /> Services
+												</NavLink>
+												<div
+													className='dropdown-menu'
+													aria-labelledby='navbarDropdown_1'
+												>
+													<NavLink
+														className='dropdown-item'
+														to='/Services/Listes'
+													>
+														{' '}
+														Services List
+													</NavLink>
+													<NavLink
+														className='dropdown-item'
+														to='/Services/Categories'
+													>
+														Categories
+													</NavLink>
+												</div>
+											</li>
 
-        )
-    }
+											<li className='nav-item dropdown'>
+												<NavLink
+													className='nav-link dropdown-toggle'
+													to='/blog'
+													id='navbarDropdown_2'
+													role='button'
+													data-toggle='dropdown'
+													aria-haspopup='true'
+													aria-expanded='false'
+													activeStyle={{
+														color: '#795376 ',
+														borderBottom: '3px solid #795376',
+													}}
+												>
+													<IoMegaphoneOutline /> blog
+												</NavLink>
+												<div
+													className='dropdown-menu'
+													aria-labelledby='navbarDropdown_2'
+												>
+													<NavLink className='dropdown-item' exact to='/blog'>
+														{' '}
+														blog
+													</NavLink>
+													<NavLink className='dropdown-item' to='/blog/Single'>
+														Single blog
+													</NavLink>
+												</div>
+											</li>
+											<li className='nav-item'>
+												<NavLink
+													className='nav-link'
+													to='/contact'
+													activeStyle={{
+														color: '#795376 ',
+														borderBottom: '3px solid #795376',
+													}}
+												>
+													<ImProfile /> Contact
+												</NavLink>
+											</li>
+										</ul>
+									</div>
+									<div className='hearer_icon d-flex align-items-center'>
+										<a id='search_1' href='javascript:void(0)'>
+											<i className='ti-search' />
+										</a>
+										<a href='cart.html'>
+											<i className='flaticon-shopping-cart-black-shape' />
+										</a>
+									</div>
+								</nav>
+							</div>
+						</IconContext.Provider>
+					</div>
+				</div>
+				{/* <div className='search_input' id='search_input_box'>
+						<div className='container '>
+							<form className='d-flex justify-content-between search-inner'>
+								<input
+									type='text'
+									className='form-control'
+									id='search_input'
+									placeholder='Search Here'
+								/>
+								<button type='submit' className='btn' />
+								<span
+									className='ti-close'
+									id='close_search'
+									title='Close Search'
+								/>
+							</form>
+						</div>
+					</div> */}
+			</header>
+		</div>
+	);
 }
