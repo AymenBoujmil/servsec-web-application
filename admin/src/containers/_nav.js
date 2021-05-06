5d8860c6c2e41f97cc7e0687753d8749f87c7554
hamza-mahjoub <60366675+hamza-mahjoub@users.noreply.github.com>
2021-05-07 00:16:10 +0100

    Add admin module

---
+++ admin/src/containers/_nav.js
@@ -0,0 +1,33 @@
+import React from "react";
+import CIcon from "@coreui/icons-react";
+
+const _nav = [
+  {
+    _tag: "CSidebarNavItem",
+    name: "Dashboard",
+    to: "/dashboard",
+    icon: <CIcon name="cil-speedometer" customClasses="c-sidebar-nav-icon" />,
+    badge: {
+      color: "info",
+      text: "NEW",
+    },
+  },
+  {
+    _tag: "CSidebarNavTitle",
+    _children: ["Dashboard"],
+  },
+  {
+    _tag: "CSidebarNavItem",
+    name: "Categories",
+    to: "/theme/colors",
+    icon: "cil-drop",
+  },
+  {
+    _tag: "CSidebarNavItem",
+    name: "Services",
+    to: "/theme/typography",
+    icon: "cil-pencil",
+  },
+];
+
+export default _nav;
